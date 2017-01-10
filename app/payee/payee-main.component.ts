afd03204782c1170077b15d1f266c3aab471b966
johnpaxton <pax@speedingplanet.com>
2017-01-10 14:27:28 -0500

    Day 2: Payee details!

--- app/payee/payee-main.component.ts
+++ app/payee/payee-main.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Payee } from './Payee';
 
 @Component({
   selector: 'payee-main',
-  template: '<h3>The Payee component</h3>'
+  template: `
+<div class="row">
+  <h3 class="text-center">Payees</h3>
+</div>
+<payee-detail [payee]="payee"></payee-detail>`
 })
 export class PayeeMainComponent {
-
+  payee = new Payee( {
+    "payeeId"   : 47,
+    "payeeName" : "Goodman, Lieber, Kurtzberg, Holliway",
+    "categoryId": 13,
+    "address"   : "16 W 12 St.",
+    "city"      : "New York",
+    "state"     : "NY",
+    "zip"       : "10015",
+    "image"     : "/images/animals/9.jpg",
+    "motto"     : null,
+    "txs"       : null
+  } );
 }
 
--- app/payee/payee.module.ts
+++ app/payee/payee.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PayeeMainComponent } from './payee-main.component';
+import { PayeeDetailComponent } from './payee-detail.component';
 
 @NgModule( {
   imports     : [ CommonModule ],
-  declarations: [ PayeeMainComponent  ],
+  declarations: [ PayeeMainComponent, PayeeDetailComponent  ],
   exports     : [ PayeeMainComponent  ]
 } )
 export class PayeeModule {
