9299749ac80ebbda1e7c4451adb7b6cd01e8c406
johnpaxton <pax@speedingplanet.com>
2017-01-10 10:50:08 -0500

    More updates day two

--- app/tx/tx-main.component.ts
+++ app/tx/tx-main.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'tx-main',
-  template: `<h3>The Transactions component</h3>
-<tx-detail></tx-detail>`
+  template: `<div class="row">
+<h3>The Transactions component</h3>
+<tx-detail></tx-detail>
+</div>`
 })
 export class TxMainComponent {
 
