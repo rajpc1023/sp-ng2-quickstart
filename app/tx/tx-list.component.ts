432a96b5c2a476ef6b97b349ce041182b8f10536
johnpaxton <pax@speedingplanet.com>
2017-01-11 14:30:38 -0500

    Day 3: Styles and styles and styles and styles

--- app/tx/tx-list.component.ts
+++ app/tx/tx-list.component.ts
@@ -3,21 +3,47 @@ import { Transaction } from './Transaction';
 
 @Component( {
   selector   : 'tx-list',
-  templateUrl: 'app/tx/tx-list.component.html'
+  templateUrl: 'app/tx/tx-list.component.html',
+  styles     : [
+    `li {
+cursor: pointer
+}
+li:hover {
+background-color: aqua;
+}
+.selectedBG {
+background-color: yellow;
+}
+
+.selectedText {
+color: red;
+font-weight: bold;
+}
+`
+  ]
 } )
 export class TxListComponent {
   @Input()
-  txs:Transaction[];
+  txs: Transaction[];
 
   @Output()
   pick = new EventEmitter<number>();
 
-  tellParent(tx:Transaction) {
+  selectedTx: Transaction;
+
+  setClasses(tx:Transaction) {
+    if (tx === this.selectedTx) {
+      return 'selectedBG selectedText';
+    }
+  }
+
+  tellParent( tx: Transaction ) {
     console.log( 'Clicked on tx# ', tx.id );
+    this.selectedTx = tx;
     this.pick.emit( tx.id );
   }
 
-  txTrackBy(index:number, tx:Transaction) {
+  txTrackBy( index: number, tx: Transaction ) {
     return tx.id;
   }
 }
--- app/tx/tx-main.component.ts
+++ app/tx/tx-main.component.ts
@@ -8,6 +8,7 @@ import { TXS } from './mock-tx'
 } )
 export class TxMainComponent {
   txs = TXS;
+  detailTx: Transaction;
 
   setDetail( txId: number ) {
     console.log( 'TxMainComponent.setDetail(): ', txId );
@@ -21,33 +22,4 @@ export class TxMainComponent {
 
     this.detailTx = foundTx;
   }
-
-  detailTx = new Transaction( {
-    "id"        : 1,
-    "payeeId"   : 17,
-    "payee"     : {
-      "payeeId"   : 17,
-      "payeeName" : "Bauch-Stehr Medical Partners",
-      "categoryId": 8,
-      "address"   : "671 York Ave",
-      "city"      : "Baltimore",
-      "state"     : "MD",
-      "zip"       : "54952",
-      "image"     : "/images/business/3.jpg",
-      "motto"     : "Versatile optimizing support",
-      "txs"       : null
-    },
-    "amount"    : -97.74,
-    "txType"    : "Debit",
-    "txStatus"  : "Cleared",
-    "txDate"    : "2015-01-04T23:50:19.938",
-    "accountId" : 1,
-    "categoryId": 8,
-    "category"  : {
-      "categoryId"  : 8,
-      "categoryName": "Health",
-      "txs"         : null
-    }
-  } )
-
 }
