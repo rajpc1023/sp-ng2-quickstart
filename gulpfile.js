6d9f3dcaa570549a35c60b2e70f472fb1d975c05
johnpaxton <pax@speedingplanet.com>
2017-02-06 18:16:38 -0500

    More config files

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require( 'gulp' ),
+  gulpif = require( 'gulp-if' ),
+  del = require( 'del' ),
+  minimist = require( 'minimist' ),
+  exercises = 'exercises/',
+  start = '/start/',
+  finish = '/finish/',
+  app = 'app/';
+
+var options = minimist( process.argv.slice( 2 ) );
+
+gulp.task( 'clean-start', function() {
+  if ( options.dest )
+    return del( exercises + options.dest + start + app );
+} );
+
+gulp.task( 'clean-finish', function() {
+  if ( options.dest )
+    return del( exercises + options.dest + finish + app );
+} );
+
+gulp.task( 'clean-app', function() {
+  return del( app + '**/*' );
+} );
+
+gulp.task( 'app-to-finish', [ 'clean-finish' ], function() {
+  gulp.src( app + '**/*.+(ts|html|css)' )
+    .pipe( gulp.dest( exercises + options.dest + finish + app ) );
+} );
+
+gulp.task( 'finish-to-app', [ 'clean-app' ], function() {
+  if ( options.src ) {
+    gulp.src( exercises + options.src + finish + app + '**/*.+(ts|html|css)' )
+      .pipe( gulp.dest( app ) );
+  }
+} );
+
+gulp.task( 'app-to-start', [ 'clean-start' ], function() {
+  gulp.src( app + '**/*.+(ts|html|css)' )
+    .pipe( gulp.dest( exercises + options.dest + start + app ) );
+} );
+
+gulp.task( 'start-to-app', [ 'clean-app' ], function() {
+  if ( options.src ) {
+    gulp.src( exercises + options.src + start + app + '**/*.+(ts|html|css)' )
+      .pipe( gulp.dest( app ) );
+  }
+} );
