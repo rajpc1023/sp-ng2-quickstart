0bf928a950c91a6ecc3797ad00502240366a30b7
johnpaxton <github$U10>
2017-02-09 12:45:51 -0500

    Exercise 10 complete, really

---
+++ exercises/ex-10/start/app/payee/category-lookup.provider.ts
@@ -0,0 +1,101 @@
+/*
+ * The function below returns a list of category objects. Write a service that exposes
+ * a function getCategoryName() which takes an argument of a categoryId. It
+ * returns the category name associated with that categoryId.
+ *
+ * Then go to payee.module.ts and register this injectable as a provider.
+ *
+ */
+
+function getCategories() {
+  return [
+    {
+      "id": 1,
+      "categoryName": "Automotive"
+    },
+    {
+      "id": 2,
+      "categoryName": "Beauty"
+    },
+    {
+      "id": 3,
+      "categoryName": "Clothing"
+    },
+    {
+      "id": 4,
+      "categoryName": "Electronics"
+    },
+    {
+      "id": 5,
+      "categoryName": "Food"
+    },
+    {
+      "id": 6,
+      "categoryName": "Games"
+    },
+    {
+      "id": 7,
+      "categoryName": "Grocery"
+    },
+    {
+      "id": 8,
+      "categoryName": "Health"
+    },
+    {
+      "id": 9,
+      "categoryName": "Housing"
+    },
+    {
+      "id": 10,
+      "categoryName": "Industrial"
+    },
+    {
+      "id": 11,
+      "categoryName": "Jewelry"
+    },
+    {
+      "id": 12,
+      "categoryName": "Kids"
+    },
+    {
+      "id": 13,
+      "categoryName": "Legal"
+    },
+    {
+      "id": 14,
+      "categoryName": "Medical"
+    },
+    {
+      "id": 15,
+      "categoryName": "Movies"
+    },
+    {
+      "id": 16,
+      "categoryName": "Music"
+    },
+    {
+      "id": 17,
+      "categoryName": "Outdoors"
+    },
+    {
+      "id": 18,
+      "categoryName": "Restaurants"
+    },
+    {
+      "id": 19,
+      "categoryName": "Salary"
+    },
+    {
+      "id": 20,
+      "categoryName": "Tools"
+    },
+    {
+      "id": 21,
+      "categoryName": "Toys"
+    },
+    {
+      "id": 22,
+      "categoryName": "Utilities"
+    }
+  ];
+}
