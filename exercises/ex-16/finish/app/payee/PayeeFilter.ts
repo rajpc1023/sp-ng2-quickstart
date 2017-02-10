8c3b4c7e7ad7c5ef1c71fb649366dc5a7bd86791
johnpaxton <github$U10>
2017-02-10 17:21:27 -0500

    was missing?

---
+++ exercises/ex-16/finish/app/payee/PayeeFilter.ts
@@ -0,0 +1,8 @@
+/*
+ * Add three fields here, corresponding to the three fields you will add to
+ * the view: payeeName, city, and state. All of them are strings.
+ *
+ * Then return to payees-list.component.ts.
+ *
+ */
+export class PayeeFilter { }
