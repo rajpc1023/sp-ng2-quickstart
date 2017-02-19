f04d2ac88a4ee43cf0e8126fd252eccddbdd6d11
johnpaxton <github$U10>
2017-02-19 16:40:03 -0500

    Found missing directions

--- exercises/ex-11/finish/app/payee/payees-dao.provider.ts
+++ exercises/ex-11/finish/app/payee/payees-dao.provider.ts
@@ -1,3 +1,22 @@
+/*
+ * Import the array of Payees from mock-payees.ts
+ *
+ * Create an Injectable DAO, PayeesDAO. It should have the following methods:
+ *
+ * Name: findById
+ * Arguments: id, integer
+ * Returns: A Promise which resolves to the Payee with the specified id
+ * findById(id:int):Payee
+ *
+ * Name: list
+ * Arguments: none
+ * Returns: A Promise which resolves to an array of Payees
+ * list():Payee[]
+ *
+ * When the service is complete, go to payee.module.ts and register it.
+ *
+ */
+
 import { Injectable } from '@angular/core';
 import { PAYEES } from './mock-payees';
 import { Payee } from './Payee';
--- exercises/ex-11/start/app/payee/payees-dao.provider.ts
+++ exercises/ex-11/start/app/payee/payees-dao.provider.ts
@@ -1,23 +1,18 @@
-import { Injectable } from '@angular/core';
-import { PAYEES } from './mock-payees';
-import { Payee } from './Payee';
-
-@Injectable()
-export class PayeesDAO {
-  private payees: Payee[] = PAYEES;
-
-  findById( id ) {
-    let foundPayee = {};
-    this.payees.some( payee => {
-      if ( payee.id === id ) {
-        foundPayee = payee;
-        return true;
-      }
-    } );
-    return Promise.resolve( foundPayee );
-  }
-
-  list() {
-    return Promise.resolve( this.payees );
-  }
-}
+/*
+ * Import the array of Payees from mock-payees.ts
+ *
+ * Create an Injectable DAO, PayeesDAO. It should have the following methods:
+ *
+ * Name: findById
+ * Arguments: id, integer
+ * Returns: A Promise which resolves to the Payee with the specified id
+ * findById(id:int):Payee
+ *
+ * Name: list
+ * Arguments: none
+ * Returns: A Promise which resolves to an array of Payees
+ * list():Payee[]
+ *
+ * When the service is complete, go to payee.module.ts and register it.
+ *
+ */
